6aab4e44db492790288d5a8f46cd4f0590465f9d
Shourav paul <paul.shourav01@gmail.com>
2023-05-22 01:30:51 +0600

    first commit

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-
 const express = require('express')
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
+
 const app = express()
 app.use(cors())
 app.use(express.json())
-const port = 5000;
+const port =process.env.PORT || 5000;
 
 
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.8sp76yj.mongodb.net/?retryWrites=true&w=majority`;
@@ -34,7 +34,6 @@ async function run() {
 
         app.post('/add', async(req,res)=>{
             const result=await toysCollection.insertOne(req.body)
-            // console.log(req.body);
             res.send(result)
         });
         app.delete('/deleteToy/:id',async(req,res)=>{
@@ -116,10 +115,10 @@ async function run() {
 run().catch(console.dir);
 
 
-app.get('/', ( res) => {
+app.get('/', (req, res) => {
     res.send('hello world')
 })
 
-app.listen(5000, () => {
+app.listen(port, () => {
     console.log("Server is running 5000");
 })
